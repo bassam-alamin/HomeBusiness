48c6df4c8ef42b687b9aa1440f755d40af980ffb
bassam-alamin <bassamalchinsky96@gmail.com>
2022-06-26 22:35:30 +0300

    ft:Requirements.txt

--- Cakes/urls.py
+++ Cakes/urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+
+from django.conf import settings
 from . import views
 
 app_name = 'Cakes'
@@ -8,3 +11,7 @@ urlpatterns = [
     # url(r'^$', views.home.as_view(), name='home'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+    urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
